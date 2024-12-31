4615ae8a8bb45224ef4352df36ca10d139802d2d
sumu <2038035593@qq.com>
2025-01-01 00:08:47 +0800

    自动安装交叉编译工具链

---
+++ push_repo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# * =====================================================
+# * Copyright © hk. 2022-2025. All rights reserved.
+# * File name  : push_repo.sh
+# * Author     : 苏木
+# * Date       : 2025-01-01
+# * ======================================================
+##
+
+git add .
+git commit -a --amend
---
+++ push_tag.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# * =====================================================
+# * Copyright © hk. 2022-2025. All rights reserved.
+# * File name  : 1.sh
+# * Author     : 苏木
+# * Date       : 2025-01-01
+# * ======================================================
+##
+
+# latest_tag=`git tag | sort -V | tail -1`
+
+git push origin --delete V0.0.6
+git tag -d V0.0.6
+
